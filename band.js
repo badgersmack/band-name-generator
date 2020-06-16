1df15fe5e29ff9f94f6fadecec6ac02acb55c9f0
Luke Chant <luke_chant@yahoo.co.uk>
2020-06-16 19:33:43 +0100

    setup and ideas

---
+++ band.js
@@ -0,0 +1,5 @@
+//going to lean on some npm here, several metal ones already exist
+// alternatively do a specific set of pretentious arrays
+// then maybe bang that on npm in future
+// somehow incorporate this: https://www.npmjs.com/package/ridiculous-moon-event
+//maybe a random moon on the page?
